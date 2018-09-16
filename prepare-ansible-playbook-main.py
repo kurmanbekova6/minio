315c4eb79ce533cdea19a89b99d57b593c2f20f0
Neil Spink <neilspink@gmail.com>
2018-09-16 14:58:47 +0200

    python script to prepare main script

---
+++ prepare-ansible-playbook-main.py
@@ -0,0 +1,20 @@
+# 1 command line parameter the port number is needed
+# utility then gets the subutai container ip addresses
+# adds them to the main play book which at the end might look like this
+#     minio_server_cluster_nodes:
+#      - 'http://172.31.234.4:9091/minio-data'
+#      - 'http://172.28.234.3:9091/minio-data'
+
+import re
+import sys
+s = [re.findall('ansible_ssh_host=.*',line)
+        for line in open('/etc/ansible/minio-master.hosts')]
+if s:
+        with open("ansible-playbook-main.yml", "a") as myfile:
+                myfile.write("\n")
+                for i in s:
+                        if len(i) > 0:
+                                ip = i[0].replace("ansible_ssh_host=", "")
+                                myfile.write("      - '" + ip + ":" + sys.argv[0] + "/minio-data'")
+                                myfile.write("\n")
+                myfile.close()
